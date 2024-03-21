ffe144b685e204f21405a2a82bf78cb35a1c7263
Thiago Ubiratan <thiago.ubiratan@solucaosistemas.net>
2024-03-21 11:22:04 -0300

    adcionado paginação na grid de meses

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { getList, showUpdate, showDelete } from './billingCycleActions'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { getList, showUpdate, showDelete } from './billingCycleActions';
 
 class BillingCycleList extends Component {
-    componentWillUnmount() {
-        this.props.getList()
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 1,
+            perPage: 10 // Defina o número de registros por página conforme necessário
+        };
+    }
+
+    componentDidMount() {
+        this.props.getList();
     }
 
     renderRows() {
-        const list = this.props.list || []
-        return list.map(x => (
+        const { list } = this.props;
+        const { currentPage, perPage } = this.state;
+        const startIndex = (currentPage - 1) * perPage;
+        const endIndex = startIndex + perPage;
+        const currentList = list.slice(startIndex, endIndex);
+
+        return currentList.map(x => (
             <tr key={x._id}>
                 <td>{x.name}</td>
                 <td>{x.month}</td>
@@ -24,11 +37,34 @@ class BillingCycleList extends Component {
                     </button>
                 </td>
             </tr>
-        ))
+        ));
+    }
+
+    renderPagination() {
+        const { list } = this.props;
+        const { currentPage, perPage } = this.state;
+        const totalPages = Math.ceil(list.length / perPage);
+        const pages = Array.from(Array(totalPages).keys()).map(page => (
+            <li key={page + 1} className={`page-item ${currentPage === page + 1 ? 'active' : ''}`}>
+                <a className="page-link" onClick={() => this.setState({ currentPage: page + 1 })}>{page + 1}</a>
+            </li>
+        ));
+        return (
+            <div className="card-footer clearfix">
+                <ul className="pagination pagination-sm m-0 float-right">
+                    <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                        <a className="page-link" onClick={() => this.setState({ currentPage: currentPage - 1 })}>«</a>
+                    </li>
+                    {pages}
+                    <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                        <a className="page-link" onClick={() => this.setState({ currentPage: currentPage + 1 })}>»</a>
+                    </li>
+                </ul>
+            </div>
+        );
     }
 
     render() {
-        console.log(this.props.list)
         return (
             <div>
                 <table className='table'>
@@ -44,11 +80,16 @@ class BillingCycleList extends Component {
                         {this.renderRows()}
                     </tbody>
                 </table>
+                {this.renderPagination()}
             </div>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => ({ list: state.billingCycle.list })
-const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate, showDelete }, dispatch)
+const mapStateToProps = state => ({
+    list: state.billingCycle.list
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
+
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
