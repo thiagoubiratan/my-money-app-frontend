a23293c1eb7d6dc07b9dd3f54332e2c3f05f3139
AMCOM\thiago.lima <thiago.lima@amcom.com.br>
2024-10-01 18:50:07 -0300

    ajustes configracao API

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,31 +1,32 @@
-import axios from 'axios'
-import { toastr } from 'react-redux-toastr'
-import { reset as resetForm, initialize } from 'redux-form'
-import { showTabs, selectTab } from '../common/tab/tabActions'
+import axios from 'axios';
+import { toastr } from 'react-redux-toastr';
+import { reset as resetForm, initialize } from 'redux-form';
+import { showTabs, selectTab } from '../common/tab/tabActions';
+import config from '../config/config';  // Importando o arquivo de configuração
 
-const BASE_URL = 'http://localhost:3030/api'
+const BASE_URL = config.BASE_URL;  // Usando a URL da configuração
 
-const INITAL_VALUES = { credits: [{}], debts: [{}] }
+const INITAL_VALUES = { credits: [{}], debts: [{}] };
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
+    const request = axios.get(`${BASE_URL}/billingCycles`);
 
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
-    }
+    };
 }
 
 export function create(values) {
-    return submit(values, 'post')
+    return submit(values, 'post');
 }
 
 export function update(values) {
-    return submit(values, 'put')
+    return submit(values, 'put');
 }
 
 export function remove(values) {
-    return submit(values, 'delete')
+    return submit(values, 'delete');
 }
 
 export function showUpdate(billingCycle) {
@@ -33,7 +34,7 @@ export function showUpdate(billingCycle) {
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
         initialize('billingCycleFrom', billingCycle)
-    ]
+    ];
 }
 
 export function showDelete(billingCycle) {
@@ -41,7 +42,7 @@ export function showDelete(billingCycle) {
         showTabs('tabDelete'),
         selectTab('tabDelete'),
         initialize('billingCycleFrom', billingCycle)
-    ]
+    ];
 }
 
 export function init() {
@@ -50,19 +51,19 @@ export function init() {
         selectTab('tabList'),
         getList(),
         initialize('billingCycleFrom', INITAL_VALUES)
-    ]
+    ];
 }
 
 function submit(values, method) {
     return dispatch => {
-        const id = values._id ? values._id : ''
+        const id = values._id ? values._id : '';
         axios[method](`${BASE_URL}/billingCycles/${id}`, values)
             .then(resp => {
-                toastr.success('Sucesso', 'Operação realizada com sucesso.')
-                dispatch(init())
+                toastr.success('Sucesso', 'Operação realizada com sucesso.');
+                dispatch(init());
             })
             .catch(e => {
-                e.response.data.errors.forEach(error => toastr.error('Erro', error))
-            })
-    }
+                e.response.data.errors.forEach(error => toastr.error('Erro', error));
+            });
+    };
 }
---
+++ src/configs/config.js
@@ -0,0 +1,5 @@
+const config = {
+    BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:3030/api',
+};
+
+export default config;
