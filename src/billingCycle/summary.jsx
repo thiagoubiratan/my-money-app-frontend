aee6dc67dcf8dfe990860b4d2e06fad8db07a5b0
AMCOM\thiago.lima <thiago.lima@amcom.com.br>
2024-10-05 20:07:36 -0300

    Ajuste ao criar um novo ciclo de pagamento

--- src/billingCycle/summary.jsx
+++ src/billingCycle/summary.jsx
@@ -10,16 +10,16 @@ export default ({ credit, debt, pending, consolidateValue }) => (
             <legend>Resumo</legend>
             <Row>
                 <ValueBox cols='12 6' color='green' icon='dollar'
-                    value={`R$ ${credit}`} text='Total de Créditos'>
+                    value={`R$ ${credit || 0}`} text='Total de Créditos'>
                 </ValueBox>
                 <ValueBox cols='12 6' color='red' icon='credit-card'
-                    value={`R$ ${debt}`} text='Total de Débitos'>
+                    value={`R$ ${debt || 0}`} text='Total de Débitos'>
                 </ValueBox>
                 <ValueBox cols='12 6' color='yellow' icon='balance-scale'
-                    value={`R$ ${pending}`} text='Pendente Pagamento'>
+                    value={`R$ ${pending || 0}`} text='Pendente Pagamento'>
                 </ValueBox>
                 <ValueBox cols='12 6' color='blue' icon='money'
-                    value={`R$ ${consolidateValue}`} text='Valor Consolidado'>
+                    value={`R$ ${consolidateValue || 0}`} text='Valor Consolidado'>
                 </ValueBox>
             </Row>
         </fieldset>
