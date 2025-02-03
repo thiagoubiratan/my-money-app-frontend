437d61ac85a78eefad593b96f08fc63286c61644
AMCOM\thiago.lima <thiago.lima@amcom.com.br>
2025-02-03 11:52:26 -0300

    ajuste abreviacao do nome do projeto

--- src/common/template/header.jsx
+++ src/common/template/header.jsx
@@ -4,7 +4,7 @@ import NavBar from './navBar';
 export default props => (
     <header className='main-header'>
         <a href='/#/' className='logo'>
-            <span className='logo-mini'><b>G</b>360</span>
+            <span className='logo-mini'><b>MD</b>360</span>
             <span className='logo-lg'>
                 <i className='fa fa-balance-scale'></i>
                 <b> Meu Dinheiro</b> 360
