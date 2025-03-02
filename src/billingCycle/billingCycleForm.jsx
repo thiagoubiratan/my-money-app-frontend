1361f5072cc3e4571f4ffa38c798eb71b11abc5d
AMCOM\thiago.lima <thiago.lima@amcom.com.br>
2025-03-01 21:40:18 -0300

    Atualização botão copia, para a tela de listagem de despesas

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -10,23 +10,10 @@ import Summary from './summary';
 class BillingCycleForm extends Component {
     constructor(props) {
         super(props);
-        this.handleCopy = this.handleCopy.bind(this); // Bind manual para o método
-    }
-
-    handleCopy() {
-        const { billingCycleId, copyBillingCycle } = this.props;
-        if (billingCycleId) {
-            copyBillingCycle(billingCycleId); // Chama a ação para copiar o ciclo de pagamento
-        } else {
-            console.error('ID do ciclo de pagamento não encontrado.');
-        }
     }
 
     render() {
         const { handleSubmit, readOnly, credits, debts, summaryCredits, summaryDebts, summaryPending, summaryConsol, billingCycleId } = this.props;
-
-        // Mostrar o botão de cópia somente se billingCycleId existir e o formulário não for somente leitura
-        const showCopyButton = billingCycleId && !readOnly;
 
         return (
             <form role="form" onSubmit={handleSubmit}>
@@ -47,13 +34,6 @@ class BillingCycleForm extends Component {
                     <button type="button" className="btn btn-warning" onClick={this.props.init}>
                         Cancelar
                     </button>
-                    
-                    {/* Condicional para mostrar o botão de cópia */}
-                    {showCopyButton && (
-                        <button type="button" className="btn btn-default" onClick={this.handleCopy}>
-                            Fazer cópia
-                        </button>
-                    )}
                 </div>
             </form>
         );
--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,115 +1,142 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { getList, showUpdate, showDelete } from './billingCycleActions';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getList, showUpdate, showDelete, copyBillingCycle } from "./billingCycleActions";
 
 class BillingCycleList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentPage: 1,
-            perPage: 10 // Defina o número de registros por página conforme necessário
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      perPage: 10, // Defina o número de registros por página conforme necessário
+    };
+    this.handleCopy = this.handleCopy.bind(this); // Bind manual para o método
+  }
+
+  handleCopy(billingCycleId) {
+    const { copyBillingCycle } = this.props;
+    if (billingCycleId) {
+      copyBillingCycle(billingCycleId); // Chama a ação para copiar o ciclo de pagamento
+    } else {
+      console.error("ID do ciclo de pagamento não encontrado.");
     }
+  }
 
-    componentDidMount() {
-        this.props.getList();
-    }
+  componentDidMount() {
+    this.props.getList();
+  }
 
-    renderRows() {
-        const { list } = this.props;
-        const { currentPage, perPage } = this.state;
-        const startIndex = (currentPage - 1) * perPage;
-        const endIndex = startIndex + perPage;
-        const currentList = list.slice(startIndex, endIndex);
+  renderRows() {
+    const { list } = this.props;
+    const { currentPage, perPage } = this.state;
+    const startIndex = (currentPage - 1) * perPage;
+    const endIndex = startIndex + perPage;
+    const currentList = list.slice(startIndex, endIndex);
 
-        return currentList.map(x => (
-            <tr key={x._id}>
-                <td>{x.name}</td>
-                <td>{x.month}</td>
-                <td>{x.year}</td>
-                <td>
-                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(x)}>
-                        <i className='fa fa-pencil'></i>
-                    </button>
-                    <button className='btn btn-danger' onClick={() => this.props.showDelete(x)}>
-                        <i className='fa fa-trash-o'></i>
-                    </button>
-                </td>
+    return currentList.map((x) => (
+      <tr key={x._id}>
+        <td>{x.name}</td>
+        <td>{x.month}</td>
+        <td>{x.year}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => this.props.showUpdate(x)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => this.props.showDelete(x)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <button
+            className="btn bg-purple"
+            onClick={() => this.handleCopy(x._id)} // Passa o ID do ciclo de pagamento para a função handleCopy
+          >
+            <i className="fa fa-copy"></i>
+          </button>
+        </td>
+      </tr>
+    ));
+  }
+
+  renderPagination() {
+    const { list } = this.props;
+    const { currentPage, perPage } = this.state;
+    const totalPages = Math.ceil(list.length / perPage);
+    const pages = Array.from(Array(totalPages).keys()).map((page) => (
+      <li
+        key={page + 1}
+        className={`page-item ${currentPage === page + 1 ? "active" : ""}`}
+      >
+        <a
+          className="page-link"
+          style={{ cursor: "pointer" }} // Estilo inline aqui
+          onClick={() => this.setState({ currentPage: page + 1 })}
+        >
+          {page + 1}
+        </a>
+      </li>
+    ));
+
+    return (
+      <div className="card-footer clearfix">
+        <ul className="pagination pagination-sm m-0 float-right">
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <a
+              className="page-link"
+              style={{ cursor: "pointer" }} // Estilo inline aqui
+              onClick={() => this.setState({ currentPage: currentPage - 1 })}
+            >
+              «
+            </a>
+          </li>
+          {pages}
+          <li
+            className={`page-item ${
+              currentPage === totalPages ? "disabled" : ""
+            }`}
+          >
+            <a
+              className="page-link"
+              style={{ cursor: "pointer" }} // Estilo inline aqui
+              onClick={() => this.setState({ currentPage: currentPage + 1 })}
+            >
+              »
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Mês</th>
+              <th>Ano</th>
+              <th className="table-actions">Ações</th>
             </tr>
-        ));
-    }
-
-    renderPagination() {
-        const { list } = this.props;
-        const { currentPage, perPage } = this.state;
-        const totalPages = Math.ceil(list.length / perPage);
-        const pages = Array.from(Array(totalPages).keys()).map(page => (
-            <li key={page + 1} className={`page-item ${currentPage === page + 1 ? 'active' : ''}`}>
-                <a 
-                  className="page-link" 
-                  style={{ cursor: 'pointer' }}  // Estilo inline aqui
-                  onClick={() => this.setState({ currentPage: page + 1 })}
-                >
-                  {page + 1}
-                </a>
-            </li>
-        ));
-        
-        return (
-            <div className="card-footer clearfix">
-                <ul className="pagination pagination-sm m-0 float-right">
-                    <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                        <a 
-                          className="page-link" 
-                          style={{ cursor: 'pointer' }}  // Estilo inline aqui
-                          onClick={() => this.setState({ currentPage: currentPage - 1 })}
-                        >
-                          «
-                        </a>
-                    </li>
-                    {pages}
-                    <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                        <a 
-                          className="page-link" 
-                          style={{ cursor: 'pointer' }}  // Estilo inline aqui
-                          onClick={() => this.setState({ currentPage: currentPage + 1 })}
-                        >
-                          »
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
-    
-
-    render() {
-        return (
-            <div>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Mês</th>
-                            <th>Ano</th>
-                            <th className='table-actions'>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderRows()}
-                    </tbody>
-                </table>
-                {this.renderPagination()}
-            </div>
-        );
-    }
+          </thead>
+          <tbody>{this.renderRows()}</tbody>
+        </table>
+        {this.renderPagination()}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    list: state.billingCycle.list
+const mapStateToProps = (state) => ({
+  list: state.billingCycle.list,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ getList, showUpdate, showDelete, copyBillingCycle }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
