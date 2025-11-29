52e47444d19467c169082ee0dfb81da6cf9506a0
Thiago Ubiratan <thiagoubiratansl@gmail.com>
2025-11-29 15:58:42 -0300

    atualizacao do icone do sistema

--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -14,7 +14,14 @@ import Input from '../common/form/inputAuth'
 class Auth extends Component {
     constructor(props) {
         super(props)
-        this.state = { loginMode: true }
+        this.state = { loginMode: true, mobileBlocked: false }
+    }
+
+    componentDidMount() {
+        const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        if (isMobile) {
+            this.setState({ mobileBlocked: true });
+        }
     }
 
     changeMode() {
@@ -22,12 +29,13 @@ class Auth extends Component {
     }
 
     onSubmit(values) {
+        if (this.state.mobileBlocked) return; // segurança extra
         const { login, signup } = this.props
         this.state.loginMode ? login(values) : signup(values)
     }
 
     render() {
-        const { loginMode } = this.state
+        const { loginMode, mobileBlocked } = this.state
         const { handleSubmit } = this.props
 
         return (
@@ -36,16 +44,31 @@ class Auth extends Component {
                     <i className='fa fa-bank'></i>
                     <b> ESFin</b>
                 </div>
+
                 <div className="login-box-body">
                     <p className="login-box-msg">Bem vindo!</p>
+
+                    {mobileBlocked && (
+                        <div className="alert alert-danger">
+                            <i className="fa fa-warning" style={{ marginRight: 6 }}></i>
+                            O acesso por dispositivos móveis não é permitido.  
+                            Por favor, utilize um computador.
+                        </div>
+                    )}
+
                     <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
                         <Field component={Input} type="input" name="name" placeholder="Nome" icon='user' hide={loginMode} />
                         <Field component={Input} type='email' name='email' placeholder='E-mail' icon='envelope' />
                         <Field component={Input} type="password" name="password" placeholder="Password" icon='lock' />
                         <Field component={Input} type="password" name="confirm_password" placeholder="Confirmar password" icon='lock' hide={loginMode} />
+
                         <Row>
                             <Grid cols="4">
-                                <button type="submit" className="btn btn-primary btn-block btn-flat">
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary btn-block btn-flat"
+                                    disabled={mobileBlocked}
+                                >
                                     <i className="fa fa-sign-in" style={{ marginRight: 5 }}></i>
                                     {loginMode ? 'Entrar' : 'Registrar'}
                                 </button>
@@ -53,13 +76,7 @@ class Auth extends Component {
                         </Row>
                     </form>
 
-                    <br />
-
-                    <a onClick={() => this.changeMode()} style={{ cursor: 'pointer' }}>
-                        {loginMode ? 'Novo usuário? Registrar aqui!' : 'Já é cadastrado? Entrar aqui!'}
-                    </a>
-
-                    {loginMode && (
+                    {!mobileBlocked && loginMode && (
                         <p style={{ marginTop: '10px' }}>
                             <a href="#/forgot-password">Esqueceu sua senha?</a>
                         </p>
@@ -71,6 +88,7 @@ class Auth extends Component {
         )
     }
 }
+
 
 Auth = reduxForm({ form: 'authForm' })(Auth)
 const mapDispatchToProps = dispatch => bindActionCreators({ login, signup }, dispatch)
--- src/common/template/header.jsx
+++ src/common/template/header.jsx
@@ -5,11 +5,11 @@ export default props => (
     <header className='main-header'>
         <a href='/#/' className='logo'>
             <span className='logo-mini'>
-                <i className='fa fa-dollar'></i>
+                <i className='fa fa-bank'></i>
                 <b></b>
             </span>
             <span className='logo-lg'>
-                <i className='fa fa-dollar'></i>
+                <i className='fa fa-bank'></i>
                 <b> ESFin </b>
             </span>
         </a>
